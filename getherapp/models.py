247825bda37cb8b45396ec2bac98533ca4135780
주영빈 <tonty1222@naver.com>
2022-06-05 22:42:58 +0900

    최신화

--- getherapp/models.py
+++ getherapp/models.py
@@ -1,27 +1,42 @@
 from django.db import models
 
 # Create your models here.
-
 class Class(models.Model):
-    class_id = models.IntegerField()
+    # class_id = models.IntegerField()
     class_name = models.CharField(max_length=50)
+    class_date1 = models.CharField(max_length=20, null=True, blank=True)
+    class_date2 = models.CharField(max_length=20, null=True, blank=True)
+    class_time1 = models.TimeField(max_length=50, null=True, blank=True)
+    class_time2 = models.TimeField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return self.class_name
 
 class Professor(models.Model):
-    professor_id = models.IntegerField()
+    # professor_id = models.IntegerField()
     professor_name = models.CharField(max_length=20)
     
-class Classroom(models.Model):
-    classroom_id = models.IntegerField()
-    classroom_unit = models.CharField(max_length=20)
-    professor_name = models.ForeignKey(Professor, on_delete = models.SET_NULL, null=True)
-    class_name = models.ForeignKey(Class, on_delete=models.CASCADE, null=True)
+    def __str__(self):
+        return self.professor_name
 
+class Classroom(models.Model):
+    class_name = models.ForeignKey(Class, on_delete=models.CASCADE, null=True)
+    professor_name = models.ForeignKey(Professor, on_delete = models.SET_NULL, null=True)
+    class_date1 = models.ForeignKey(Class, on_delete = models.SET_NULL, null=True, related_name = 'date1')
+    class_date2 = models.ForeignKey(Class, on_delete = models.SET_NULL, null=True, related_name = 'date2')
+    class_time1 = models.ForeignKey(Class, on_delete = models.SET_NULL, null=True, related_name = 'time1')
+    class_time2 = models.ForeignKey(Class, on_delete = models.SET_NULL, null=True, related_name = 'time2')
+    # classroom_id = models.IntegerField()
+    classroom_unit = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.classroom_unit
 
 class Feedback(models.Model):
-    user_id = models.IntegerField()
+    user_id = models.CharField(max_length=50)
     feedback_date = models.DateTimeField(auto_now_add=True)
     user_phonenum = models.CharField(max_length=100, null=True) #추가해봤어염
     feedback_content = models.TextField()
+
+    def __str__(self):
+        return self.user_id
--- getherapp/views.py
+++ getherapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from .forms import FeedbackForm
 
 # Create your views here.
 def base(request):
@@ -8,43 +10,64 @@ def index(request):
     return render(request, 'index.html')
 
 def feedback(request):
-    return render(request, 'feedback.html')
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.feedback_date = timezone.now()
+            form.save()
+            return redirect('index')
+    else:
+        form = FeedbackForm()
+        return render(request, 'feedback.html', {'form': form})
 
 def introduce(request):
     return render(request, 'introduce.html')
 
-def dormitory(request):
-    return render(request, 'class/dormitory.html')
-
-def gdin_gwan(request):
-    return render(request, 'class/gdin_gwan.html')
-
-def im_gwan(request):
-    return render(request, 'class/im_gwan.html')
-
-def jg_gwan(request):
-    return render(request, 'class/jg_gwan.html')
-
-def library(request):
-    return render(request, 'class/library.html')
-
-def mgell_gwan(request):
-    return render(request, 'class/mgell_gwan.html')
-
-def nn_gwan(request):
-    return render(request, 'class/nn_gwan.html')
-
-def pb_hall(request):
-    return render(request, 'class/pb_hall.html')
-
-def sbdr_school(request):
-    return render(request, 'class/sbdr_school.html')
-
-def scn_gwan(request):
-    return render(request, 'class/scn_gwan.html')
-
+# 1관 승연관
 def sy_gwan(request):
     return render(request, 'class/sy_gwan.html')
 
+# 2관 일만관
+def im_gwan(request):
+    return render(request, 'class/im_gwan.html')
+
+# 3관 월당관
 def wd_gwan(request):
     return render(request, 'class/wd_gwan.html')
+
+# 5관 나눔관
+def nn_gwan(request):
+    return render(request, 'class/nn_gwan.html')
+
+# 6관 정보과학관
+def jg_gwan(request):
+    return render(request, 'class/jg_gwan.html')
+
+# 7관 새천년관
+def scn_gwan(request):
+    return render(request, 'class/scn_gwan.html')
+
+# 8관 중앙도서관
+def library(request):
+    return render(request, 'class/library.html')
+
+# 9관 피츠버그홀
+def pb_hall(request):
+    return render(request, 'class/pb_hall.html')
+
+# 10관 구두인관
+def gdin_gwan(request):
+    return render(request, 'class/gdin_gwan.html')
+
+# 11관 성베드로학교
+def sbdr_school(request):
+    return render(request, 'class/sbdr_school.html')
+
+# 12관 미가엘관
+def mgell_gwan(request):
+    return render(request, 'class/mgell_gwan.html')
+
+# 13관 행복기숙사
+def dormitory(request):
+    return render(request, 'class/dormitory.html')
