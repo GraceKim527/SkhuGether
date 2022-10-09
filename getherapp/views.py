c044db81dc718aa9834c5f01d3e6760ef0f4a18e
Rommmu <tonty1222@naver.com>
2022-10-09 23:56:56 +0900

    time_table

--- getherapp/views.py
+++ getherapp/views.py
@@ -16,37 +16,27 @@ now_weekday = days[weekday]
 
 am9 = time(9, 00, 00, 000000)
 am9_30 = time(9, 30, 00, 000000)
-am9_50 = time(9, 50, 00, 000000)
 am10 = time(10, 00, 00, 000000)
 am10_30 = time(10, 30, 00, 000000)
-am10_50 = time(10, 50, 00, 000000)
 am11 = time(11, 00, 00, 000000)
 am11_30 = time(11, 30, 00, 000000)
-am11_50 = time(11, 50, 00, 000000)
 pm12 = time(12, 00, 00, 000000)
 pm12_30 = time(12, 30, 00, 000000)
-pm12_50 = time(12, 50, 00, 000000)
 pm1 = time(13, 00, 00, 000000)
 pm1_30 = time(13, 30, 00, 000000)
-pm1_50 = time(13, 50, 00, 000000)
 pm2 = time(14, 00, 00, 000000)
 pm2_30 = time(14, 30, 00, 000000)
-pm2_50 = time(14, 50, 00, 000000)
 pm3 = time(15, 00, 00, 000000)
 pm3_30 = time(15, 30, 00, 000000)
-pm3_50 = time(15, 50, 00, 000000)
 pm4 = time(16, 00, 00, 000000)
 pm4_30 = time(16, 30, 00, 000000)
-pm4_50 = time(16, 50, 00, 000000)
 pm5 = time(17, 00, 00, 000000)
 pm5_30 = time(17, 30, 00, 000000)
-pm5_50 = time(17, 50, 00, 000000)
 pm6 = time(18, 00, 00, 000000)
 pm6_30 = time(18, 30, 00, 000000)
-pm6_50 = time(18, 50, 00, 000000)
 pm7 = time(19, 00, 00, 000000)
 
-time_list = [am9, am9_30, am9_50, am10, am10_30, am10_50, am11, am11_30, am11_50, pm12, pm12_30, pm12_50, pm1, pm1_30, pm1_50, pm2, pm2_30, pm2_50, pm3, pm3_30, pm3_50, pm4, pm4_30, pm4_50, pm5, pm5_30, pm5_50, pm6, pm6_30, pm6_50, pm7]
+time_list = [am9, am9_30, am10, am10_30, am11, am11_30, pm12, pm12_30, pm1, pm1_30, pm2, pm2_30, pm3, pm3_30, pm4, pm4_30, pm5, pm5_30, pm6, pm6_30,pm7]
 
 def base(request):
     return render(request, 'base.html')
@@ -151,7 +141,8 @@ def dormitory(request):
 
 def classroom(request, unit_id, id):
     classroom = get_object_or_404(Classroom, unit = unit_id, id = id)
-    classes = Class.objects.all()
+    classes = Class.objects.all().order_by('time2')
+
     return render(request, 'classroom.html', 
     {'now_date':now_date, 'now_time':now_time, 'now_weekday':now_weekday, 
     'time_list': time_list, 
