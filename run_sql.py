349cb1fc6c982e01d97787243608ff7b7ab9e0a8
김수진 <ksj030120@gmail.com>
2022-10-14 21:38:49 +0900

    edit 2

--- run_sql.py
+++ run_sql.py
@@ -7,8 +7,6 @@ def my_custom_sql(sql_file_name):
     with connection.cursor() as cursor:
         sql_file = open(sql_file_name, encoding='UTF-8')
         sql_as_string = sql_file.read()
-        cursor.executescript(sql_as_string)
-        cursor.close()
-        connection.close()
+        cursor.executeScript(sql_as_string)
 
 my_custom_sql("model.db.sql")
