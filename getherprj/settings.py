944bd9c5213787653ac4f804d5c529e98f44d0c0
김수진 <ksj030120@gmail.com>
2022-10-15 23:13:30 +0900

    1015 deploy

--- getherprj/settings.py
+++ getherprj/settings.py
@@ -138,8 +138,3 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-#Heroku
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
